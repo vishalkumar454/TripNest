212464675f3110132558351a2e8c1f48abcfb98e
vishalkumar <rajvishal71760@gmail.com>
2026-08-15 21:16:06 +0530

    for performing updation and deletion i installed method-override dependency

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const Listing = require("./models/listings.js")
+const methodOverride = require("method-override")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/tripnest"
 
@@ -9,6 +10,7 @@ const path = require("path")
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended : true}))
+app.use(methodOverride("_method"))
 
 main().then(() => console.log("Connected to DB"))
     .catch((err) => console.log(err))
@@ -18,14 +20,18 @@ async function main() {
 }
 
 app.get("/",(req,res) => {
-    res.send("hii, i am root")
+    res.send("hii, i am Tripnest page")
 })
 
 // index route
 app.get("/listings",async (req,res) => {
-    let allListings = await Listing.find({});
-    console.log("data fetched")
+    let allListings = await Listing.find({})
     res.render("./listings/index.ejs",{allListings})
+})
+
+// New route
+app.get("/listings/new",(req,res) => {
+    res.render("./listings/new.ejs")
 })
 
 // show route
@@ -33,6 +39,35 @@ app.get("/listings/:id",async (req,res) => {
     let {id} = req.params;
     let listing = await Listing.findById(id)
     res.render("./listings/show.ejs",{listing})
+})
+
+// create route
+app.post("/listings",async (req,res) => {
+    const newListing = await new Listing(req.body.listing)
+    await newListing.save()
+    res.redirect("/listings")
+})
+
+// edit route
+app.get("/listings/:id/edit",async (req,res) => {
+    let {id} = req.params
+    let userData = await Listing.findById(id);
+    res.render("./listings/edit.ejs",{userData})
+})
+
+// update route
+app.put("/listings/:id",async (req,res) => {
+    let {id} = req.params
+    let listing = req.body.listing
+    await Listing.findByIdAndUpdate(id,listing)
+    res.redirect(`/listings/${id}`)
+})
+
+// delete route
+app.delete("/listings/:id",async (req,res) => {
+    let {id} = req.params
+    await Listing.findByIdAndDelete(id)
+    res.redirect("/listings")
 })
 
 app.listen(8080,() => {
