01859135f5ddeee746a1f29f6810f3aa72a6e4d8
vishalkumar <rajvishal71760@gmail.com>
2026-08-23 19:54:18 +0530

    new route is implemented

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended : true}))
 app.use(methodOverride("_method"))
 app.engine("ejs", ejsMate)
+app.use(express.static(path.join(__dirname,"/public")))
 
 main().then(() => console.log("Connected to DB"))
     .catch((err) => console.log(err))
